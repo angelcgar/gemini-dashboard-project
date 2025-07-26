0fac6c9fdc67aa80eece97b0b05d2ad4718a8fd5
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2025-07-26 14:56:30 -0600

    feat: implement sidebar component with context and mobile support
    - Added SidebarProvider and SidebarTrigger for managing sidebar state.
    - Created AppSidebar component with navigation items.
    - Introduced UI components: Button, Input, Separator, Sheet, Skeleton, Tooltip.
    - Implemented useIsMobile hook for responsive design.
    - Refactored App component to integrate sidebar functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css';
 import WeeklyBitcoinChart from './components/charts/WeeklyBitcoinChart';
 
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+
 function App() {
 	return (
-		<div className="bg-gray-900 min-h-screen text-white font-sans">
-			<header className="bg-gray-800 shadow-md p-4">
-				<h1 className="text-3xl font-bold text-center text-green-400">
+		<SidebarProvider>
+			<AppSidebar />
+			{/* <div className="bg-gray-900 min-h-screen text-white font-sans w-full"> */}
+			{/* <header className="bg-gray-800 shadow-md p-4">
+					<h1 className="text-3xl font-bold text-center text-green-400">
 					Crypto Dashboard
-				</h1>
-			</header>
-			<main className="container mx-auto p-4 sm:p-6 lg:p-8">
+					</h1>
+					</header> */}
+			<main className="bg-gray-900 min-h-screen text-white font-sans w-full">
+				<SidebarTrigger />
 				<div className="grid grid-cols-1 gap-6">
 					<WeeklyBitcoinChart />
 					{/* Aquí irían los otros componentes del dashboard */}
@@ -17,7 +23,8 @@ function App() {
 					{/* <TopTenChangesBarChart /> */}
 				</div>
 			</main>
-		</div>
+			{/* </div> */}
+		</SidebarProvider>
 	);
 }
 
